23fdf658812a98ea9293f06b8ba0ce8b94bfe439
KarelDiaz <kdiazalfonso94@gmail.com>
2021-06-17 17:08:32 -0400

    Add players to team model

--- src/store/model/team.js
+++ src/store/model/team.js
@@ -1,8 +1,10 @@
 export default class Team {
   name;
+  players;
   tournament;
   constructor(name = "", tournament = {}) {
     this.name = name;
+    this.players = [];
     this.tournament = tournament;
   }
 }

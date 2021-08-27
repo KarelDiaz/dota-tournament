0a7aa48f4d4fa152ef2eb2fcef1560570f79c66e
Karel Diaz <kdiazalfonso94@gmail.com>
2021-08-27 12:40:40 -0400

    Virtual view

---
+++ src/views/Virtual.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-center space-x-3">
+    <input type="number" v-model="a" />
+    <input type="number" v-model="b" />
+    <span>{{ mmrA }}</span>
+    <span>{{ mmrB }}</span>
+  </div>
+</template>
+
+<script>
+import MMR from "@/store/model/mmr";
+
+export default {
+  data() {
+    return {
+      a: 0,
+      b: 0,
+    };
+  },
+  computed: {
+    mmrA() {
+      return new MMR(this.a, this.b).getPlusA();
+    },
+    mmrB() {
+      return new MMR(this.a, this.b).getPlusB();
+    },
+  },
+};
+</script>
+
+<style>
+</style>

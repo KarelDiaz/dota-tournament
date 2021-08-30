d07ae21aa57b4183e12e16dc3718f3e454641b5f
Karel Diaz <kdiazalfonso94@gmail.com>
2021-08-30 11:31:20 -0400

    RankComponent

---
+++ src/components/RankComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img
+      :class="'h-' + size"
+      :src="`rank/${rank.name}.png`"
+      :title="getRank(mmr).name"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import { GET_RANK } from "@/store/type/getters";
+
+export default {
+  data() {
+    return {
+      rank: {},
+    };
+  },
+  props: {
+    mmr: Number,
+    size: { type: Number, default: 5 },
+  },
+  computed: {
+    ...mapGetters([GET_RANK]),
+  },
+  mounted() {
+    this.rank = this.getRank(this.mmr);
+  },
+};
+</script>
